9ee510f7c04915ae24013c8326bbb52a8dcfb8da
truuue <noahvernhet@gmail.com>
2024-08-15 14:14:16 +0200

    Update webpack and jQuery dependencies, fix code formatting, and improve Holberton Dashboard content

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,22 @@
-import $ from "jquery";
-import _ from "lodash";
+
+import $ from 'jquery';
+import _ from 'lodash';
+
+const content = [
+    '<p>Holberton Dashboard</p>',
+    '<p>Dashboard data for the students</p>',
+    "<button>Click here to get started</button>",
+    "<p id='count'></p>",
+    "<p>Copyright - Holberton School</p>"
+]
+
+content.forEach(i => $("body").append(i))
 
 let count = 0;
 
 function updateCounter() {
-  count += 1;
-  $("#count").text(`${count} clicks on the button`);
+    count += 1;
+    document.getElementById("count").innerHTML = `${count} clicks on the button`;
 }
 
-$(document).ready(function () {
-  $("body").append("<p>Holberton Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<button>Click here to get started</button>");
-  $("body").append('<p id="count"></p>');
-  $("body").append("<p>Copyright - Holberton School</p>");
-
-  $("button").on("click", _.debounce(updateCounter, 500));
-});
+$("button").on('click', _.debounce(updateCounter, 500))
