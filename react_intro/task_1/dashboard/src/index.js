b0eacd2016641597a1dd0b81c3a8d653948b977a
truuue <noahvernhet@gmail.com>
2024-08-21 11:55:43 +0200

    Refactor root rendering in index.js

--- react_intro/task_1/dashboard/src/index.js
+++ react_intro/task_1/dashboard/src/index.js
@@ -7,8 +7,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div className="root-notifications">
-      <App />
       <Notifications />
     </div>
+    <App />
   </React.StrictMode>
 );
