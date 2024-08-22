b2f4dedaa9c95ee1b57d2e7d98f9a9d913d27a3c
truuue <noahvernhet@gmail.com>
2024-08-22 11:16:10 +0200

    4. Props types & Default props & Shapes

---
+++ react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  let element;
+
+  if (isHeader === true) {
+    if (textSecondCell === null) {
+      element = <th colSpan="2">{textFirstCell}</th>;
+    } else {
+      element = (
+        <>
+          <th>{textFirstCell}</th>
+          <th>{textSecondCell}</th>
+        </>
+      );
+    }
+    //
+  } else if (isHeader === false) {
+    element = (
+      <>
+        <td>{textFirstCell}</td>
+        <td>{textSecondCell}</td>
+      </>
+    );
+  }
+
+  return <tr>{element}</tr>;
+}
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
+};
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.string,
+};
+
+export default CourseListRow;
