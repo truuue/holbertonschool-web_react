6c0877f4e35dc773090c24e747773d0cec7027d8
truuue <noahvernhet@gmail.com>
2024-10-11 16:55:10 +0200

    task 10

---
+++ react_redux_connectors_and_providers/task_9/dashboard/src/Notifications/NotificationsContainer.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NotificationsContainer } from "./NotificationsContainer";
+import { shallow } from "enzyme";
+
+// Mocking the fetchNotifications function
+describe("<NotificationsContainer />", () => {
+  it("verify that the function fetchNotifications is called when the component is mounted", () => {
+    const fetchNotifications = jest.fn();
+
+    const wrapper = shallow(
+      <NotificationsContainer fetchNotifications={fetchNotifications} />
+    );
+
+    expect(fetchNotifications).toHaveBeenCalled();
+
+    jest.restoreAllMocks();
+  });
+});
